0374ab72da0756052f789d0f63cb9c0b75127885
eszx (home_comp) <adlab@mail.ru>
2021-03-18 19:46:20 +0300

    Убрали лишние методы.

--- app/src/main/java/ezsx/sum/MainActivity.java
+++ app/src/main/java/ezsx/sum/MainActivity.java
@@ -8,8 +8,12 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] digits = { "0","0","0","0","0","0","0","0","0","0","0","0", };
-    String[] digits1= { "0","0","0","0","0","0","0","0","0","0","0","0", };
+    private String[] digits = { "0","0","0","0","0","0","0","0","0","0","0","0", };
+    private String[] digits1= { "0","0","0","0","0","0","0","0","0","0","0","0", };
+    private String s1;
+    private String s2;
+    private String s3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,223 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void button_0(View view) {
+    public void button_all(View view) {
+        ToggleButton bt =  ((ToggleButton) view);
+        Object t = bt.getTag();
+        String s = String.valueOf(t);
+        if (s==null) return;
+        int i = Integer.valueOf(s);
         // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
+        boolean on = bt.isChecked();
         if (on) {
-            digits[0] = "1";
+            if (i <12)
+                digits[i] = "1";
+            else
+                digits1[i-12] = "1";
         } else {
-            digits[0] = "0";
+            if (i <12)
+                digits[i] = "0";
+            else
+                digits1[i-12] = "0";
         }
+        calc_digits();
     }
-    public void button_1(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[1] = "1";
-        } else {
-            digits[1] = "0";
-        }
+
+    private void calc_digits() {
+        s1 = digits[0] + digits[1] + digits[2] + digits[3] + digits[4] + digits[5] + digits[6] + digits[7] + digits[8] + digits[9] + digits[10] + digits[11];
+        s2 = digits1[0] + digits1[1] + digits1[2] + digits1[3] + digits1[4] + digits1[5] + digits1[6] + digits1[7] + digits1[8] + digits1[9] + digits1[10] + digits1[11];
+        s3 = Integer.toBinaryString(Integer.parseInt(s1, 2) + Integer.parseInt(s2, 2));
+        TextView txtv_s1 = (TextView) findViewById(R.id.s1);
+        TextView txtv_s2 = (TextView) findViewById(R.id.s2);
+        TextView txtv_s3 = (TextView) findViewById(R.id.s3);
+        txtv_s1.setText(s1);
+        txtv_s2.setText(s2);
+        txtv_s3.setText(s3);
     }
-    public void button_2(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[2] = "1";
-        } else {
-            digits[2] = "0";
-        }
-    }
-    public void button_3(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[3] = "1";
-        } else {
-            digits[3] = "0";
-        }
-    }
-    public void button_4(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[4] = "1";
-        } else {
-            digits[4] = "0";
-        }
-    }
-    public void button_5(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[5] = "1";
-        } else {
-            digits[5] = "0";
-        }
-    }
-    public void button_6(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[6] = "1";
-        } else {
-            digits[6] = "0";
-        }
-    }
-    public void button_7(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[7] = "1";
-        } else {
-            digits[7] = "0";
-        }
-    }
-    public void button_8(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[8] = "1";
-        } else {
-            digits[8] = "0";
-        }
-    }
-    public void button_9(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[9] = "1";
-        } else {
-            digits[9] = "0";
-        }
-    }
-    public void button_10(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[10] = "1";
-        } else {
-            digits[10] = "0";
-        }
-    }
-    public void button_11(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits[11] = "1";
-        } else {
-            digits[11] = "0";
-        }
-    }
-    public void button_12(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[0] = "1";
-        } else {
-            digits1[0] = "0";
-        }
-    }
-    public void button_13(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[1] = "1";
-        } else {
-            digits1[1] = "0";
-        }
-    }
-    public void button_14(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[2] = "1";
-        } else {
-            digits1[2] = "0";
-        }
-    }
-    public void button_15(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[3] = "1";
-        } else {
-            digits1[3] = "0";
-        }
-    }
-    public void button_16(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[4] = "1";
-        } else {
-            digits1[4] = "0";
-        }
-    }
-    public void button_17(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[5] = "1";
-        } else {
-            digits1[5] = "0";
-        }
-    }
-    public void button_18(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[6] = "1";
-        } else {
-            digits1[6] = "0";
-        }
-    }
-    public void button_19(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[7] = "1";
-        } else {
-            digits1[7] = "0";
-        }
-    }
-    public void button_20(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[8] = "1";
-        } else {
-            digits1[8] = "0";
-        }
-    }
-    public void button_21(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[9] = "1";
-        } else {
-            digits1[9] = "0";
-        }
-    }
-    public void button_22(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[10] = "1";
-        } else {
-            digits1[10] = "0";
-        }
-    }
-    public void button_23(View view) {
-        // включена ли кнопка
-        boolean on = ((ToggleButton) view).isChecked();
-        if (on) {
-            digits1[11] = "1";
-        } else {
-            digits1[11] = "0";
-        }
-    }
-    String s1 =digits[0]+digits[1]+digits[2]+digits[3]+digits[4]+digits[5]+digits[6]+digits[7]+digits[8]+digits[9]+digits[10]+digits[11];
-    String s2 =digits1[0]+digits1[1]+digits1[2]+digits1[3]+digits1[4]+digits1[5]+digits1[6]+digits1[7]+digits1[8]+digits1[9]+digits1[10]+digits1[11];
-    String s = Integer.toBinaryString(Integer.parseInt(s1, 2) + Integer.parseInt(s2, 2));
 }
